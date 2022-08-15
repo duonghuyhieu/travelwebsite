fde923385a82c99019371a4f849199f2147494e2
Dương Huy HIếu <69691417+duonghuyhieu@users.noreply.github.com>
2022-08-15 23:03:27 +0700

    Update SignUpPage.js

--- src/components/signup/SignUpPage.js
+++ src/components/signup/SignUpPage.js
@@ -2,12 +2,10 @@ import React from 'react'
 import './SignUpPage.css'
 
 function SignUpPage() {
-
-
-  const [params, setParams] = React.useState({email: '', password:''})
-  const [errors, setErros] =  React.useState({errEmail: "", errPassword: ""})
-  const  {email, password} = params; 
-  const { errEmail, errPassword} = errors
+  const [params, setParams] = React.useState({email: '', password:'', confirmPassword:''})
+  const [errors, setErros] =  React.useState({errEmail: "", errPassword: "", errConfirmPassword : ""})
+  const  {email, password, confirmPassword} = params; 
+  const { errEmail, errPassword, errConfirmPassword} = errors
 
   const handleOnChange = (e) => {
     setParams( prev => ({...prev, [e.target.id]: e.target.value}))
@@ -26,13 +24,18 @@ function SignUpPage() {
       setErros(prev => ({...prev, errPassword: "Invalid password"}))
     } else { setErros(prev => ({...prev, errPassword: ""}))}
 
+    if (password != confirmPassword){
+      result = false
+      setErros(prev => ({...prev, errConfirmPassword: "Password don't match"}))
+    }else { setErros(pre => ({...pre, errConfirmPassword: ""}))}
+
     return result
   }
 
   const handleSubmit = () => {
     console.log(params)
     if(handleValidate()) {
-      console.log('login')
+      console.log('signup')
     } else { return}
   }
   return (
@@ -41,10 +44,10 @@ function SignUpPage() {
       <h1> TRAVEL</h1>
       <div className='signup__select signup__row'>
         <div className='signup__box signup__select__signin' >
-          <p>Sign in</p>
+          <p>Sign up</p>
         </div>
           <div className='signup__box' >
-            <p>Sign Up</p>
+            <p>Sign in</p>
           </div>
         </div>
         <div className='signup__form signup__col'>
@@ -57,8 +60,13 @@ function SignUpPage() {
           <input type='password' onChange={handleOnChange} id='password' placeholder='Enter password'></input>
           { errPassword && <span>{errPassword}</span>}
         </div>
-        <p className='signup__passwordretrieval'>Password retrieval</p>
-        <button onClick={handleSubmit} className='signup__btn'>Sign in<i className='fas fa-sign-in-alt'></i></button>
+        <div className='signup__form signup__col'>
+          <label>Confirm password <span>*</span></label>
+          <input type='password' onChange={handleOnChange} id='confirmPassword' placeholder='Confirm password'></input>
+          { errConfirmPassword && <span>{errConfirmPassword}</span>}
+        </div>
+        {/* <p className='signup__passwordretrieval'>Password retrieval</p> */}
+        <button onClick={handleSubmit} className='signup__btn'>Sign up<i className='fas fa-sign-in-alt'></i></button>
         <p className='signup__or'>Or</p>
         <div className='signup__siwith'>
           <div className='signup__box signup__facebook'><i className='fab fa-facebook'></i> Continue with Facebook</div>
